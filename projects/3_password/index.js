93c240adfb9677c3bb4611c0f4efa85b193f821e
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2021-10-30 15:17:49 +0200

    password generator

--- projects/3_password/index.js
+++ projects/3_password/index.js
@@ -1,1 +1,110 @@
+const resultDOM = document.getElementById('result');
+const copybtnDOM = document.getElementById('copy');
+const lengthDOM = document.getElementById('length');
+const uppercaseDOM = document.getElementById('uppercase');
+const numbersDOM = document.getElementById('numbers');
+const symbolsDOM = document.getElementById('symbols');
+const generatebtn = document.getElementById('generate');
+const form = document.getElementById('passwordGenerator');
 
+// charactere du mdp
+const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
+
+// copier mdp
+copybtnDOM.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const passwordToCopy = resultDOM.innerText;
+
+  if (!passwordToCopy) return;
+
+  // fonction copier
+  textarea.value = passwordToCopy;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('Mot de passe généré copié ;)');
+});
+
+// vérifié/checker les options sélectionnées (=uppercase, number, symbol) et mise en place du mdp
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const characterAmount = lengthDOM.value;
+  const includeUppercase = uppercaseDOM.checked;
+  const includeNumbers = numbersDOM.checked;
+  const includeSymbols = symbolsDOM.checked;
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+  resultDOM.innerText = password;
+});
+
+// Generer mot de passe
+let generatePassword = (
+  characterAmount,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) => {
+  let charCodes = LOWERCASE_CODES;
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CODES);
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CODES);
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CODES);
+  const passwordCharacters = [];
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode =
+      charCodes[Math.floor(Math.random() * charCodes.length)];
+    passwordCharacters.push(String.fromCharCode(characterCode));
+  }
+  return passwordCharacters.join('');
+};
+
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
+
+/* const alpha = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
+
+
+const passwordTxt = document.getElementById("password");
+const length = document.getElementById("length");
+const incNumbers = document.getElementById("numbers");
+const incSymbols = document.getElementById("symbols");
+const incUppercase = document.getElementById("uppercase");
+const generateBtn = document.getElementById("generate");
+
+
+generateBtn.addEventListener("click", () => {
+    let characters = alpha;
+    incNumbers.checked ? (characters += numbers) : "";
+    incSymbols.checked ? (characters += symbols) : "";
+    incUppercase.checked ? (characters += uppercase) : "";
+    passwordTxt.value = generatePassword(length.value, characters);
+  });
+
+const generatePassword = (length, characters) => {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return password;
+}; */
